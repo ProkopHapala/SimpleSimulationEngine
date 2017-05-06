d65307643225c8b1344f6eb0f3a77433878722fa
ProkopHapala <ProkopHapala@gmail.com>
2017-05-06 22:01:21 +0200

    test_SpaceFlightODE tested for realistic parameters

--- cpp/apps/OrbitalWar/test_SpaceFlightODE.cpp
+++ cpp/apps/OrbitalWar/test_SpaceFlightODE.cpp
@@ -31,20 +31,24 @@ double  thetaCPs [nCP];
 double  thrustCPs[nCP];
 Vec3d   dirCPs   [nCP];
 
-double  uT    = 1.0; // duration of each CP interval
+double  uT    = 50.0; // duration of each CP interval
 double inv_uT = 1.0/uT;
 
 // rocket parameters - Saturn V
 double  vexhaust      = 4500.0; // [km/s]
 double  dm_F          = 1.0/vexhaust;
-double  mass_initial  = 2.970e+6    *0.0; // put there zero to switch off the engine
+double  mass_initial  = 2.970e+6; // put there zero to switch off the engine
 double  mass_empty    = 130e+3 + 140e+3 + 40e+3 + 13.5e+3;
 double  thrust_full   = 35e+6;
 
 // planet parameters
 const double planet_R   = 6371e+3;
-double       planet_GM  = 3.9860044189+14; //https://en.wikipedia.org/wiki/Standard_gravitational_parameter
+double       planet_GM  = 3.9860044189e+14; //https://en.wikipedia.org/wiki/Standard_gravitational_parameter
 Vec3d        planet_pos = {0.0,-planet_R,0.0};
+
+
+
+double view_scale = 1e-6;
 
 // ==== Functions
 
@@ -53,7 +57,7 @@ inline double getSpline( double u, double* CPs ){
     double cp1 = CPs[1];
     double cp2 = CPs[2];
     double cp3 = CPs[3];
-    return Spline_Hermite::val( u, cp1, cp2, (cp2-cp0)*0.5d, (cp2-cp1)*0.5d );
+    return Spline_Hermite::val( u, cp1, cp2, (cp2-cp0)*0.5d, (cp3-cp1)*0.5d );
 }
 
 inline Vec3d getSpline3d( double u, Vec3d* CPs ){
@@ -64,9 +68,9 @@ inline Vec3d getSpline3d( double u, Vec3d* CPs ){
     Vec3d cp2 = CPs[2];
     Vec3d cp3 = CPs[3];
     return { // TODO Optimize this
-        Spline_Hermite::val( u, cp1.x, cp2.x, (cp2.x-cp0.x)*0.5d, (cp2.x-cp1.x)*0.5d ),
-        Spline_Hermite::val( u, cp1.y, cp2.y, (cp2.y-cp0.y)*0.5d, (cp2.y-cp1.y)*0.5d ),
-        Spline_Hermite::val( u, cp1.z, cp2.z, (cp2.z-cp0.z)*0.5d, (cp2.z-cp1.z)*0.5d )
+        Spline_Hermite::val( u, cp1.x, cp2.x, (cp2.x-cp0.x)*0.5d, (cp3.x-cp1.x)*0.5d ),
+        Spline_Hermite::val( u, cp1.y, cp2.y, (cp2.y-cp0.y)*0.5d, (cp3.y-cp1.y)*0.5d ),
+        Spline_Hermite::val( u, cp1.z, cp2.z, (cp2.z-cp0.z)*0.5d, (cp3.z-cp1.z)*0.5d )
     };
 }
 
@@ -88,7 +92,7 @@ inline void addGravity( const Vec3d& dp, Vec3d& G, double GM ){
     double ir2 = 1.0d/dp.norm2();
     double ir  = sqrt( ir2 );
     double ir3 = ir2*ir;
-    G.add_mul( dp, ir3*GM);
+    G.add_mul( dp, -ir3*GM);
 }
 
 void getAcceleration( double t, int n, double * Ys, double * dYs ){
@@ -105,14 +109,25 @@ void getAcceleration( double t, int n, double * Ys, double * dYs ){
         // --- interpolate from inputs
         double tcp = t*inv_uT;
         int    icp = (int) tcp;
-        double u   = tcp - icp;
+        if((icp+3)<nCP){
+            double u   = tcp - icp;
 
-        // TODO : this can be very much optimized if we evaluate spline basis instead !!!!
-        Vec3d  dir    = getSpline3d( u, dirCPs    );
-        double thrust = getSpline  ( u, thrustCPs );
+            // TODO : this can be very much optimized if we evaluate spline basis instead !!!!
+            Vec3d  dir    = getSpline3d( u, dirCPs   +icp );
+            double thrust = getSpline  ( u, thrustCPs+icp );
 
-        acc.add_mul( dir, thrust/m );
-        dm =     dm_F * thrust; // propelant consumption
+            //glColor3f(0.0f,0.0f,0.0f); Draw3D::drawVecInPos( dir*5.0, pos*view_scale );
+            glColor3f(0.0f,0.0f,0.0f); Draw3D::drawLine( pos*view_scale, pos*view_scale + dir );
+            //Vec3d dir_ = {1.0,dir.y,0.0};
+            //glColor3f(0.0f,0.0f,0.0f); Draw3D::drawLine( pos*view_scale, pos*view_scale + dir_ );
+            //printf( "t %g tcp %g icp %i u %g (%g,%g,%g) \n", t, tcp, icp, u, dir.x,dir.y,dir.z  );
+
+            dm =     -dm_F * thrust; // propelant consumption
+
+            //printf( "T %g T/m %g acc %g m %g dm %g \n", thrust,thrust/m, acc.norm(), m, dm  );
+            acc.add_mul( dir, thrust/m );
+        }
+        //exit(0);
     };
 
     // TODO : air drag
@@ -127,7 +142,7 @@ void getAcceleration( double t, int n, double * Ys, double * dYs ){
 
 class TestApp_SpaceFlightODE : public AppSDL2OGL_3D {
 	public:
-
+    bool RUNNING = true;
     ODEintegrator_RKF45 odeint;
 
     Vec3d  *pos=NULL,*vel=NULL,*acc=NULL;
@@ -149,26 +164,26 @@ TestApp_SpaceFlightODE::TestApp_SpaceFlightODE( int& id, int WIDTH_, int HEIGHT_
 
     odeint.reallocate( 7 );
     odeint.getDerivs = getAcceleration;
-    odeint.dt_max   = 0.5;
-    odeint.dt_min   = 0.01;
-    odeint.dt_adapt = 0.1;
+    odeint.dt_max   = 0.005;
+    odeint.dt_min   = 0.0001;
+    odeint.dt_adapt = 0.001;
 
-    ((Vec3d*)(odeint.invMaxYerr  ))->set(1e+8);
-    ((Vec3d*)(odeint.invMaxYerr+3))->set(1e+8);
-    odeint.invMaxYerr[6] = 1e+8;
+    ((Vec3d*)(odeint.invMaxYerr  ))->set(1/1e-1);
+    ((Vec3d*)(odeint.invMaxYerr+3))->set(1/1e-2);
+    odeint.invMaxYerr[6] = 1/1e+1;
 
     pos  = (Vec3d*)(odeint.Y   );
     vel  = (Vec3d*)(odeint.Y+3 );
     acc  = (Vec3d*)(odeint.dY+3);
     mass = odeint.Y+6;
 
-    pos->set(1.0,0.0,0.0); // pos
-    vel->set(0.0,1.3,0.0); // vel
+    pos->set(1.0,0.0,0.0); vel->set(0.0,0.0,0.0);
+    //pos->set(0.0,100.0e+3,0.0); vel->set(7.9e+3,0.0,0.0); satelite
     *mass = mass_initial;
 
     for(int i=0; i<nCP; i++){
         double t = (i-1)*0.5;
-        double theta   = M_PI_2/(1.0d + t );
+        double   theta = M_PI_2/(1.0d + t*t*1.5 );
         if(i==0) theta = M_PI_2;
         thetaCPs [i] = theta;
         thrustCPs[i] = thrust_full;
@@ -177,13 +192,17 @@ TestApp_SpaceFlightODE::TestApp_SpaceFlightODE( int& id, int WIDTH_, int HEIGHT_
 
     glClearColor( 0.5f, 0.5f, 0.5f, 1.0f );
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    //zoom = 0.5;
 }
 
 void TestApp_SpaceFlightODE::draw(){
     //printf( " ==== frame %i \n", frameCount );
-    //glClearColor( 0.5f, 0.5f, 0.5f, 1.0f );
-	//glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glClearColor( 0.5f, 0.5f, 0.5f, 1.0f );
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
+	/*
+	// --- fade out
 	glDisable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     //glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
@@ -191,36 +210,50 @@ void TestApp_SpaceFlightODE::draw(){
 	glColor4f(0.5f,0.5f,0.5f, 0.05f);Draw2D::drawRectangle( {-20.0,-20.0},{20.0,20.0}, true);
 	//glDisable(GL_BLEND);
 	//glClear( GL_DEPTH_BUFFER_BIT );
+    */
 
 	// --- draw control spline
-    //glColor3f(1.0f,1.0f,1.0f); Draw3D::drawCurve( 0.0, 8.0, 100, splinePixel );
+    glColor3f(1.0f,1.0f,1.0f); Draw3D::drawCurve( 0.0, 8.0, 100, splinePixel );
     //for(int i=0; i<nCP; i++){ Draw3D::drawPointCross( {i,thetaCPs[i]*5.0,0.0}, 0.1 ); }
-    //for(int i=0; i<nCP; i++){ Draw3D::drawPointCross( dirCPs[i], 0.1 ); }
+    for(int i=0; i<nCP; i++){ Draw3D::drawPointCross( dirCPs[i], 0.1 ); }
+
+    //RUNNING = 0;
+
+    if(RUNNING){
+        int nstep = 0;
+        //odeint.step( 0.1d );
+        //odeint.adaptive_step_RKF45( );
+        //nstep = odeint.integrate_adaptive( odeint.dt_adapt, odeint.t+0.2d );
+        nstep = odeint.integrate_adaptive( odeint.dt_adapt, odeint.t+5.0d );
+
+        //odeint.step_RKF45( 0.1d );
+        //odeint.step_euler( 0.1d );
+
+        //printf( "%i %i (%g,%g,%g) %g %g\n ", frameCount, nstep, pos->x,pos->y,pos->z, odeint.t, odeint.dt_adapt );
+        printf( " %g : (%g,%g,%g) %g %g \n ", odeint.t, pos->x,pos->y,pos->z, vel->norm(), *mass );
+        //glColor3f(0.0f,0.0f,0.0f); Draw3D::drawLine    ( opos      , *pos ); Draw3D::drawPointCross( *pos, 0.1 );
+        //glColor3f(0.0f,0.0f,1.0f); Draw3D::drawVecInPos( (*vel)*2.0, *pos );
+
+        if( (*pos-planet_pos).norm2()<(planet_R*planet_R) ){
+            printf("crashed !!!\n");
+            RUNNING = false;
+        }
+    }
 
 
-    double view_scale = 1e-6;
-
-
-	int nstep = 0;
-	//odeint.step( 0.1d );
-	//odeint.adaptive_step_RKF45( );
-	nstep = odeint.integrate_adaptive( odeint.dt_adapt, odeint.t+0.2d );
-	//odeint.step_RKF45( 0.1d );
-	//odeint.step_euler( 0.1d );
-
-	//printf( "%i %i (%g,%g,%g) %g %g\n ", frameCount, nstep, pos->x,pos->y,pos->z, odeint.t, odeint.dt_adapt );
-	printf( " %g : (%g,%g,%g) %g \n ", odeint.t, pos->x,pos->y,pos->z, *mass );
-    //glColor3f(0.0f,0.0f,0.0f); Draw3D::drawLine    ( opos      , *pos ); Draw3D::drawPointCross( *pos, 0.1 );
-    //glColor3f(0.0f,0.0f,1.0f); Draw3D::drawVecInPos( (*vel)*2.0, *pos );
 
     glColor3f(0.0f,0.0f,0.0f); Draw3D::drawLine    ( opos*view_scale      , (*pos)*view_scale ); Draw3D::drawPointCross( *pos*view_scale, 0.1 );
     glColor3f(0.0f,0.0f,1.0f); Draw3D::drawVecInPos( (*vel)*2.0*view_scale, (*pos)*view_scale );
 
     opos = *pos;
 
+
+
     //glColor3f(0.0f,0.0f,0.0f); Draw3D::drawPointCross( {0.0,0.0,0.0}, 0.5 );
     glColor3f(0.0f,0.0f,0.0f); Draw3D::drawPointCross( {0.0,0.0,0.0}, 0.1 );
     glColor3f(0.0f,0.0f,0.0f); Draw3D::drawPointCross( planet_pos*view_scale, 0.5 );
+
+    glColor3f(0.0f,0.0f,0.0f); Draw3D::drawCircleAxis( 64, planet_pos*view_scale, {0.0,planet_R*view_scale,0.0}, {0.0,0.0,1.0} );
 
 	glDisable ( GL_LIGHTING );
 
