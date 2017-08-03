99f7eb2d230adb64484d640590416a31dcf35cdb
ProkopHapala <ProkopHapala@gmail.com>
2017-08-03 21:58:44 +0200

    spaceshil1.lua - skeatch how scripting of spaceship can looklike

---
+++ cpp/apps/OrbitalWar/data/spaceshil1.lua
@@ -0,0 +1,55 @@
+﻿--- https://www.lua.org/pil/5.3.html
+
+newSpaceShip( "Ship1" )
+
+origin = {0.0,0.0,0.0}
+xvec   = {1.0,0.0,0.0} 
+yvec   = {1.0,0.0,0.0}
+zvec   = {1.0,0.0,0.0}
+
+n0 = Node( origin )
+n1 = Node( {-100.0,   0.0,    0.0} )
+n2 = Node( {+100.0,   0.0,    0.0} )
+n3 = Node( {   0.0,-200.0,    0.0} )
+n4 = Node( {   0.0,+200.0,    0.0} )
+n5 = Node( {   0.0,   0.0, -300.0} )
+n6 = Node( {   0.0,   0.0, +800.0} )
+
+--         from to  Up nseg,mseg   width, hegith 
+g1 = Girder( n0, n1, zvec, 10, 2, {10.0,8.0} )
+g2 = Girder( n0, n2, zvec, 10, 2, {10.0,8.0} )
+g3 = Girder( n0, n3, xvec, 20, 2, {10.0,8.0} )
+g4 = Girder( n0, n4, xvec, 20, 2, {10.0,8.0} )
+g5 = Girder( n0, n5, xvec, 30, 2, {10.0,8.0} )
+g6 = Girder( n0, n6, xvec, 80, 2, {10.0,8.0} )
+
+--          type     thick[mm]
+M_rope1 = {"kevalr", 10   }
+
+Rope(n5,n1,M_rope1); Rope(n6,n1,M_rope1)
+Rope(n5,n2,M_rope1); Rope(n6,n2,M_rope1)
+Rope(n5,n3,M_rope1); Rope(n6,n3,M_rope1)
+Rope(n5,n4,M_rope1); Rope(n6,n4,M_rope1)
+
+--                type          T[K]
+M_radiator = {"LithiumHeatPipe", 1280.0 }
+
+Radiator( g5, g1 )
+Radiator( g5, g2 )
+Radiator( g5, g3 )
+Radiator( g5, g4 )
+Radiator( g6, g1 )
+Radiator( g6, g2 )
+Radiator( g6, g3 )
+Radiator( g6, g4 )
+
+Ring  ( n0, n6, xvec, 80, 2, {10.0,8.0},  nodes={} )
+
+--                   path    side offset
+Gun ( "railgun", {n5,n0,n6}, { 1.0,0.0} )
+Gun ( "XFEL",    {n5,n0,n6}, {-1.0,0.0} )
+
+Tank  ( "", R=16 )
+Tank  ( "water", R=16 )
+
+Engine( n5, maxPower=7.0e+9, maxTrust=10.0e+3, maxVexh=50e+3, nodes={} )
--- cpp/tests/lua/LuaScript.h
+++ cpp/tests/lua/LuaScript.h
@@ -11,128 +11,81 @@ extern "C" {
 # include <lua5.2/lualib.h>
 }
 
-class LuaScript {
-public:
+class LuaScript { public:
+    lua_State* L;
+    std::string filename;
+    int level;
+
     LuaScript(const std::string& filename);
     ~LuaScript();
     void printError(const std::string& variableName, const std::string& reason);
     std::vector<int> getIntVector(const std::string& name);
     std::vector<std::string> getTableKeys(const std::string& name);
-    
-    inline void clean() {
-      int n = lua_gettop(L);
-      lua_pop(L, n);
-    }
 
+    inline void clean() { int n = lua_gettop(L); lua_pop(L, n); } // clean stacks
+
+    // get variable by name string
     template<typename T>
     T get(const std::string& variableName) {
-      if(!L) {
-        printError(variableName, "Script is not loaded");
-        return lua_getdefault<T>();
-      }
-      
-      T result;
-      if(lua_gettostack(variableName)) { // variable succesfully on top of stack
-        result = lua_get<T>(variableName);  
-      } else {
-        result = lua_getdefault<T>();
-      }
-
-     
-      clean();
-      return result;
+        if(!L) { printError(variableName, "Script is not loaded"); return lua_getdefault<T>(); }
+        T result;
+        if(lua_gettostack(variableName)) {  result = lua_get<T>(variableName); }
+        else {                              result = lua_getdefault<T>();      }
+        clean();
+        return result;
     }
 
     bool lua_gettostack(const std::string& variableName) {
-      level = 0;
-      std::string var = "";
-        for(unsigned int i = 0; i < variableName.size(); i++) {
-          if(variableName.at(i) == '.') {
-            if(level == 0) {
-              lua_getglobal(L, var.c_str());
-            } else {
-              lua_getfield(L, -1, var.c_str());
-            }
-            
-            if(lua_isnil(L, -1)) {
-              printError(variableName, var + " is not defined");
-              return false;
-            } else {
-              var = "";
-              level++;
-            }
-          } else {
-            var += variableName.at(i);
-          }
+        level = 0;
+        std::string var = "";
+        for(unsigned int i = 0; i < variableName.size(); i++) { // walk down the hierarchy
+            if(variableName.at(i) == '.') {
+                if(level == 0) { lua_getglobal(L,     var.c_str()); }
+                else           { lua_getfield (L, -1, var.c_str()); }
+                if(lua_isnil(L, -1)) { printError(variableName, var + " is not defined"); return false; }
+                else                 { var = ""; level++; }
+            } else { var += variableName.at(i);  }
         }
-        if(level == 0) {
-          lua_getglobal(L, var.c_str());
-        } else {
-          lua_getfield(L, -1, var.c_str());
-        }
-        if(lua_isnil(L, -1)) {
-            printError(variableName, var + " is not defined");
-            return false;
-        }
-
-        return true;       
+        if(level == 0) { lua_getglobal(L,     var.c_str()); }
+        else           { lua_getfield (L, -1, var.c_str()); }
+        if(lua_isnil(L, -1)) { printError(variableName, var + " is not defined"); return false; }
+        return true; // success
     }
 
     // Generic get
-    template<typename T>
-    T lua_get(const std::string& variableName) {
-      return 0;
-    }
+    template<typename T> T lua_get(const std::string& variableName) { return 0; }
+    template<typename T> T lua_getdefault()                         { return 0; }
 
-    template<typename T>
-    T lua_getdefault() {
-      return 0;
-    }
-   
-private:
-    lua_State* L;
-    std::string filename;
-    int level;
 };
 
  // Specializations
 
-template <> 
+template <>
 inline bool LuaScript::lua_get<bool>(const std::string& variableName) {
     return (bool)lua_toboolean(L, -1);
 }
 
-template <> 
+template <>
 inline float LuaScript::lua_get<float>(const std::string& variableName) {
-    if(!lua_isnumber(L, -1)) {
-      printError(variableName, "Not a number");
-    }
+    if(!lua_isnumber(L, -1)) { printError(variableName, "Not a number"); }
     return (float)lua_tonumber(L, -1);
 }
 
 template <>
 inline int LuaScript::lua_get<int>(const std::string& variableName) {
-    if(!lua_isnumber(L, -1)) {
-      printError(variableName, "Not a number");
-    }
+    if(!lua_isnumber(L, -1)) { printError(variableName, "Not a number"); }
     return (int)lua_tonumber(L, -1);
 }
 
 template <>
 inline std::string LuaScript::lua_get<std::string>(const std::string& variableName) {
     std::string s = "null";
-    if(lua_isstring(L, -1)) {
-      s = std::string(lua_tostring(L, -1));
-    } else {
-      printError(variableName, "Not a string");
-    }
+    if(lua_isstring(L, -1)) {  s = std::string(lua_tostring(L, -1));     }
+    else                    {  printError(variableName, "Not a string"); }
     return s;
 }
 
-template<>
-inline std::string LuaScript::lua_getdefault<std::string>() {
-  return "null";
-}
+template<> inline std::string LuaScript::lua_getdefault<std::string>() { return "null"; }
 
 
 // =============== IMPLEMENTATION
@@ -143,7 +96,6 @@ LuaScript::LuaScript(const std::string& filename) {
         std::cout<<"Error: failed to load ("<<filename<<")"<<std::endl;
 		L = 0;
     }
-
     if(L) luaL_openlibs(L);
 }
 
@@ -158,9 +110,7 @@ void LuaScript::printError(const std::string& variableName, const std::string& r
 std::vector<int> LuaScript::getIntVector(const std::string& name) {
     std::vector<int> v;
     lua_gettostack(name.c_str());
-    if(lua_isnil(L, -1)) { // array is not found
-        return std::vector<int>();
-    }
+    if(lua_isnil(L, -1)) { return std::vector<int>(); }
     lua_pushnil(L);
     while(lua_next(L, -2)) {
         v.push_back((int)lua_tonumber(L, -1));
@@ -171,7 +121,7 @@ std::vector<int> LuaScript::getIntVector(const std::string& name) {
 }
 
 std::vector<std::string> LuaScript::getTableKeys(const std::string& name) {
-    std::string code = 
+    std::string code =
         "function getKeys(name) "
         "s = \"\""
         "for k, v in pairs(_G[name]) do "
@@ -179,27 +129,22 @@ std::vector<std::string> LuaScript::getTableKeys(const std::string& name) {
         "    end "
         "return s "
         "end"; // function for getting table keys
-    luaL_loadstring(L, 
-        code.c_str()); // execute code
-    lua_pcall(L,0,0,0);
-    lua_getglobal(L, "getKeys"); // get function
+    luaL_loadstring(L, code.c_str()); // execute code
+    lua_pcall     (L,0,0,0);
+    lua_getglobal (L, "getKeys"); // get function
     lua_pushstring(L, name.c_str());
     lua_pcall(L, 1 , 1, 0); // execute function
     std::string test = lua_tostring(L, -1);
     std::vector<std::string> strings;
     std::string temp = "";
     std::cout<<"TEMP:"<<test<<std::endl;
-    for(unsigned int i = 0; i < test.size(); i++) {     
-        if(test.at(i) != ',') {
-            temp += test.at(i);
-        } else {
-            strings.push_back(temp);
-            temp= "";
-        }
+    for(unsigned int i = 0; i < test.size(); i++) {
+        if(test.at(i) != ',') { temp += test.at(i);                }
+        else                  { strings.push_back(temp); temp= ""; }
     }
     clean();
     return strings;
 }
- 
+
 #endif
 
