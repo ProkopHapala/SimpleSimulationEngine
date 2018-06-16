3b98c83d7e90dd6d16a5ebe90e9e457f060d998e
ProkopHapala <ProkopHapala@gmail.com>
2018-06-16 19:34:15 +0200

    new spaceShip in lua; tested lua import module

---
+++ cpp/apps/OrbitalWar/data/lua/utils.lua
@@ -0,0 +1,22 @@
+function tanks( n,aOff, R, r, L, z0 )
+    for i=1,n do
+        local a = (i/n + aOff)*2*math.pi 
+        Tank( {math.cos(a)*R,math.sin(a)*R,z0}, zvec, {r,r,L}, "H2")
+    end
+end
+
+function girderFan( n, aOff, w, h, z0,z1,   nseg, thick, shielded )
+    local tipNode = Node({0.0,0.0,z1});
+    local a = aOff*2*math.pi 
+    local gd0  = Girder( tipNode, Node({math.cos(a)*w,math.sin(a)*h,z0}), xvec, nseg, 2, {thick,thick}, "steel")
+    local ogd  = gd0;
+    for i=1,n do
+        local gd=gd0
+        if i<n then
+            local a = (i/n + aOff)*2*math.pi
+            gd = Girder( tipNode, Node({math.cos(a)*w,math.sin(a)*h,z0}), xvec, nseg, 2, {thick,thick},  "steel" )
+        end
+        if shielded then Shield( ogd,0.0,1.0, gd,0.0,1.0 ) end
+        ogd = gd;
+    end
+end
