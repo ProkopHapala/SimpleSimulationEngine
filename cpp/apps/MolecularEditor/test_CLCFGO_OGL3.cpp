63ffe3a50619cb73eab89468279f737152c667c3
ProkopHapala <ProkopHapala@gmail.com>
2021-03-03 23:21:40 +0100

    test_CLCFGO_OGL3.cpp with CLCFGO.glslf shader seems to work properly now

--- cpp/apps/MolecularEditor/test_CLCFGO_OGL3.cpp
+++ cpp/apps/MolecularEditor/test_CLCFGO_OGL3.cpp
@@ -31,27 +31,35 @@ bool bRun = false;
 double dt;
 CLCFGO eff;
 
-Shader   * shader1;
+Shader   * shader1, *shClear;
 GLObject * object1;
 GLObject * object2;
 
+const int WIDTH  = 800;
+const int HEIGHT = 800;
+Vec2f resolution{WIDTH,HEIGHT};
+Vec3f light_dir {  0.1, 0.2, 0.5 };
+Vec3f lookAt    { 0.0f, 0.f,  0.f};
+Vec3f camPos    { 0.0f, 0.f, -5.f};
+float zoom = 5.0;
+float iso  = 0.001;
+
+Quat4f qCamera = Quat4fIdentity;
+
 GLfloat vertexes[4][2] = {
-	{  -0.8f,  -0.8f  },
-	{  -0.8f,   0.8f  },
-	{   0.8f,  -0.8f  },
-	{   0.8f,   0.8f  } };
+	{  -0.99f,  -0.99f  },
+	{  -0.99f,   0.99f  },
+	{   0.99f,  -0.99f  },
+	{   0.99f,   0.99f  } };
 
 SDL_Window * window     = NULL;
 SDL_GLContext   context = NULL;
 
 GLuint vao;     // vertex array object
 
-const int WIDTH  = 800;
-const int HEIGHT = 800;
 
-GLfloat resolution[2];
-//GLfloat light_dir [3] = { -0.35707142142f, -0.35707142142f, 0.7f };
-GLfloat light_dir [3] = { -0.35707142142f, 0.7f, 0.35707142142f };
+
+
 
 const int natoms_max = 100;
 int natoms = natoms_max;
@@ -97,7 +105,18 @@ void setup(){
     eff.iPauliModel = 0;
 
 
+    {
+        // MO 0
+        float sz=0.5;
+        float l = 0.25;
+        eff.ecoef[0]= 1.0; eff.epos[0].set(-l, 1.,0.);  eff.esize[0] = sz;
+        eff.ecoef[1]= 1.0; eff.epos[1].set( l, 1.,0.);  eff.esize[1] = sz;
+        // MO 1
+        eff.ecoef[2]= 1.0; eff.epos[2].set(-l,-1.,0.);  eff.esize[2] = sz;
+        eff.ecoef[3]=-1.0; eff.epos[3].set( l,-1.,0.);  eff.esize[3] = sz;
+    }
 
+    light_dir.normalize();
 
 
     object1 = new GLObject( );
@@ -106,51 +125,85 @@ void setup(){
     object1->buffs[0].setup(0,2,GL_FALSE,&vertexes[0][0],'v'); // vertexes
 	object1->init();
 
+    shClear=new Shader();
+	shClear->init( "common_resources/shaders/plain_vert.glslv", "common_resources/shaders/clear.glslf" );
+
 	shader1=new Shader();
 	shader1->init( "common_resources/shaders/plain_vert.glslv", "common_resources/shaders/CLCFGO.glslf" );
-
-	glUseProgram(shader1->shaderprogram);
+	//shader1->init( "common_resources/shaders/plain_vert.glslv", "common_resources/shaders/rayMarchZero.glslf" );
 
 }
 
 void draw(){
+    //glDisable( GL_DEPTH_TEST );
+    //glColorMask (GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+    //glDepthMask (GL_TRUE);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT  );
 
+    //double E = eff.eval();
+    //float F2 = eff.moveGD(dt);
 
-    double E = eff.eval();
-    float F2 = eff.moveGD(dt);
+    //bool bAlphaBlend = true;
+    bool bAlphaBlend = false;
+    if(bAlphaBlend){
+        glDisable( GL_DEPTH_TEST );
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }else{
+        //glDisable( GL_DEPTH_TEST );
+        glEnable( GL_DEPTH_TEST );
+        glDepthFunc( GL_LESS );
+    }
+    Mat3f mouseMat;
+    qCamera.toMatrix(mouseMat);
+    camPos = lookAt  + mouseMat.c * -5.;
 
+    GLuint uloc;
 
-	if( redraw  ){
-		glClearColor(0.0, 0.0, 1.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		resolution[0] = (float)WIDTH;
-    	resolution[1] = (float)HEIGHT;
-		GLuint uloc;
-    	uloc = glGetUniformLocation( shader1->shaderprogram, "resolution" );	glUniform2fv(uloc, 1, resolution  );
-		uloc = glGetUniformLocation( shader1->shaderprogram, "light_dir" );	    glUniform3fv(uloc, 1, light_dir   );
-		uloc = glGetUniformLocation( shader1->shaderprogram, "natoms"  );       glUniform1iv(uloc, 1, &eff.perOrb    );
-		GLuint uloc_atoms  = glGetUniformLocation( shader1->shaderprogram, "atoms"   );
-		GLuint uloc_coefs  = glGetUniformLocation( shader1->shaderprogram, "coefs"   );
-		Quat4f atoms[eff.perOrb];
-		Quat4f coefs[eff.perOrb];
-		for(int i=0; i<eff.nOrb; i++){
-            int io0 = eff.getOrbOffset(i);
-            for(int j=0; j<eff.perOrb; j++){
-                atoms[i].f = (Vec3f)eff.epos [io0+j];
-                atoms[i].e = eff.esize[io0+j];
-                coefs[i].f = Vec3fZero;
-                coefs[i].e = eff.ecoef[io0+j];
-            }
-            glUniform4fv(uloc_atoms, natoms, (float*)atoms );
-            glUniform4fv(uloc_coefs, natoms, (float*)coefs );
+    //shClear->use();
+    //uloc = glGetUniformLocation( shader1->shaderprogram, "iso" );	        glUniform1fv(uloc, 1, &iso     );
+    //uloc = glGetUniformLocation( shader1->shaderprogram, "zoom" );	        glUniform1fv(uloc, 1, &zoom     );
+    //object1->draw();
+
+    shader1->use();
+    uloc = glGetUniformLocation( shader1->shaderprogram, "iso" );	        glUniform1fv(uloc, 1, &iso     );
+    uloc = glGetUniformLocation( shader1->shaderprogram, "zoom" );	        glUniform1fv(uloc, 1, &zoom     );
+    uloc = glGetUniformLocation( shader1->shaderprogram, "lookAt" );	    glUniform3fv(uloc, 1, (float*)&lookAt      );
+    uloc = glGetUniformLocation( shader1->shaderprogram, "camPos" );	    glUniform3fv(uloc, 1, (float*)&camPos      );
+    uloc = glGetUniformLocation( shader1->shaderprogram, "resolution" );	glUniform2fv(uloc, 1, (float*)&resolution  );
+    uloc = glGetUniformLocation( shader1->shaderprogram, "light_dir" );	    glUniform3fv(uloc, 1, (float*)&light_dir   );
+    uloc = glGetUniformLocation( shader1->shaderprogram, "natoms"  );       glUniform1iv(uloc, 1, &eff.perOrb );
+    GLuint uloc_atoms  = glGetUniformLocation( shader1->shaderprogram, "atoms"   );
+    GLuint uloc_coefs  = glGetUniformLocation( shader1->shaderprogram, "coefs"   );
+    Quat4f atoms[eff.perOrb];
+    Quat4f coefs[eff.perOrb];
+    for(int i=0; i<eff.nOrb; i++){
+    //for(int i=0; i<1; i++){
+        int io0 = eff.getOrbOffset(i);
+        //for(int j=0; j<eff.perOrb; j++){
+        for(int j=0; j<2; j++){
+
+            atoms[j].f = (Vec3f)eff.epos [io0+j];
+            atoms[j].e = eff.esize[io0+j];
+            coefs[j].f = Vec3fZero;
+            coefs[j].e = eff.ecoef[io0+j];
+
+            //atoms[j].set(j*0.25,0,0,0.1);
+            //coefs[j].set(0,0,0,1.0 - 2*(j%2) );
         }
+        glUniform4fv(uloc_atoms, natoms, (float*)atoms );
+        glUniform4fv(uloc_coefs, natoms, (float*)coefs );
         glEnableVertexAttribArray(0);
-		object1->draw();
-		SDL_GL_SwapWindow(window);
-		redraw = false;
-	}
+        object1->draw();
+    }
+
+    SDL_GL_SwapWindow(window);
+    //redraw = false;
+
 }
 
+/*
 // FUNCTION ======	inputHanding
 void inputHanding(){
 	SDL_Event event;
@@ -163,13 +216,62 @@ void inputHanding(){
 		if( event.type == SDL_QUIT){ quit();  };
 	}
 }
+*/
+
+// FUNCTION ======	inputHanding
+void inputHanding(){
+    float posstep = 0.1;
+	SDL_Event event;
+
+	const Uint8 *keys = SDL_GetKeyboardState(NULL);
+	//Vec3d& v = lookAt;
+	Vec3f& v = camPos;
+    if( keys[ SDL_SCANCODE_W  ] ){ v.y += posstep; }
+	if( keys[ SDL_SCANCODE_S  ] ){ v.y -= posstep; }
+	if( keys[ SDL_SCANCODE_A  ] ){ v.x -= posstep; }
+	if( keys[ SDL_SCANCODE_D  ] ){ v.x += posstep; }
+    if( keys[ SDL_SCANCODE_Q  ] ){ v.z += posstep; }
+	if( keys[ SDL_SCANCODE_E  ] ){ v.z -= posstep; }
+
+    //if( keys[ SDL_SCANCODE_KP_PLUS   ] ){ resolution.mul(  1.1); }
+	//if( keys[ SDL_SCANCODE_KP_MINUS  ] ){ resolution.mul(1/1.1); }
+
+    if( keys[ SDL_SCANCODE_KP_PLUS   ] ){ zoom /= 1.1; }
+	if( keys[ SDL_SCANCODE_KP_MINUS  ] ){ zoom *= 1.1; }
+
+	while(SDL_PollEvent(&event)){
+		if( event.type == SDL_KEYDOWN ){
+            switch( event.key.keysym.sym ){
+                case SDLK_ESCAPE: quit(); break;
+            }
+		}
+		if( event.type == SDL_QUIT){ quit();  };
+	}
+
+	int mouseX,mouseY;
+    float mouseRotSpeed = 0.01;
+	int dmx,dmy;
+	SDL_GetMouseState( &mouseX, &mouseY );
+    Uint32 buttons = SDL_GetRelativeMouseState( &dmx, &dmy);
+    //printf( " %i %i \n", mx,my );
+    if ( buttons & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+        Quat4f q; q.fromTrackball( 0, 0, dmx*mouseRotSpeed, dmy*mouseRotSpeed );
+        //printf( " %i %i  (%3.3f,%3.3f,%3.3f,%3.3f) \n", dmx,dmy, q.x,q.y,q.z,q.w );
+        //qCamera.qmul_T( q );
+        qCamera.qmul( q );
+        //qCamera.normalize();
+    }
+    //printf( "input handling camPos(%g,%g,%g) lookAt(%g,%g,%g) \n", camPos.x,camPos.y,camPos.z,   lookAt.x,lookAt.y,lookAt.z );
+
+}
 
 void loop( int nframes ){
     for ( int iframe=1; iframe<nframes; iframe++)    {
  		if( !STOP ) draw();
 		inputHanding();
 		frameCount++;
-        SDL_Delay(100);
+        //SDL_Delay(100);
+        SDL_Delay(20);
     }
 }
 
@@ -186,6 +288,7 @@ void init(){
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); // Opengl 3.2
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2); // Opengl 3.2
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    //SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0); // Helps with glClear()
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     window = SDL_CreateWindow("Tutorial2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if ( !window ) die("Unable to create window");
--- cpp/common_resources/shaders/CLCFGO.glslf
+++ cpp/common_resources/shaders/CLCFGO.glslf
@@ -1,36 +1,33 @@
 
+
+uniform vec3  camPos;
+uniform vec3  lookAt;
+//uniform vec3  up;
+uniform float zoom;
+
 uniform vec2  resolution;
-uniform vec3  light_dir;
 
 uniform int  natoms;
 uniform vec4 atoms [100];
 uniform vec4 coefs [100];
 
-const vec3 clr_diffuse  = vec3( 0.9, 0.8, 0.7 );
-const vec3 clr_specular = vec3( 1.0, 1.0, 1.0 );
-const vec3 clr_ambient  = vec3( 0.1, 0.2, 0.3 );
+uniform float iso;
+uniform vec3 light_dir;
+
+//const float iso = 0.2;
+//const vec3 light_dir = vec3(1.0,0.0,0.0);
+
+//const float dt_default = 0.010;
+const float dt_max   = 0.100;
+const float dt_min   = 0.025;
+const float dt_slope = 0.5;
+
 
 //const float cutoff     = 6.0;
 const float cutoff2    = 16.;
 const float exp_cutoff = 0.00247875217; // exp(-cutoff2 );
 
-const float iso   = 0.2;
-const float tmin  = 50.0;
-const float tmax  = 60.0;
-const float dtmin = 0.01;
-const float dtmax = 0.1;
-
-const float dderiv = 0.01;
-
-vec2 rayPointDist( vec3 ray0, vec3 hRay, vec3 point ){
-	vec3 pt  = point - ray0;	
-	float t  = dot( hRay, pt );
-    pt      -= t * hRay;
-	float r2 = dot( pt, pt );  
-	return vec2( t, r2 );
-}
-
-float evalFunc( vec3 pos ){
+float LCAO( in vec3 pos ){
 	float sum = 0.0;
 	for(int i=0; i<natoms; i++){
 		vec3 dr     = pos - atoms[i].xyz;
@@ -45,76 +42,144 @@ float evalFunc( vec3 pos ){
 	return sum;
 }
 
-float bisec( vec3 ray0, vec3 hRay, float t, float dt ){
-	while( dt > dtmin ){
-		dt *=0.5;
-		float thalf = t+dt;
-		float val = evalFunc( ray0 + thalf*hRay );
-		if( abs(val) < iso ) t = thalf;
-	}
-	return t;
-}
-
-float rayMarch( vec3 ray0, vec3 hRay ){
-	float dt = dtmax;
-	float t  = tmin;
-	while( t<tmax ){
-		float val = evalFunc( ray0 + t*hRay );
-		if( abs(val) > iso )	break; 
-		t += dt;
-	}
-	//return t;
-	return bisec( ray0, hRay, t-dt, dt );
-}
-
-vec3 getNormal( vec3 hitpos, float val0 ){
-	return vec3(
-		evalFunc( hitpos + vec3( dderiv, 0.0, 0.0 ) ) - val0,
-		evalFunc( hitpos + vec3( 0.0, dderiv, 0.0 ) ) - val0,
-		evalFunc( hitpos + vec3( 0.0, 0.0, dderiv ) ) - val0
-	) / dderiv;
-}
-
-float integrate( vec3 ray0, vec3 hRay ){
-	float dt = dtmax;
-	float t  = tmin;
-	float sum = 0.0;
-	while( t<tmax ){
-		sum += evalFunc( ray0 + t*hRay );
-		t   += dt;
-	}
-	return sum;
+float Func( in vec3 pos ){
+    float f = LCAO(pos);
+    return iso-f*f;
+    //return abs(LCAO(pos))-iso;
 }
 
 
+/*
+float Func_( in vec3 pos ){
+    //return exp( -dot(pos,pos) )  - 0.2;
+    vec3  sinp = sin(pos*vec3(5.0,5.0,5.0));
+    //vec3  cosp = cos(pos*vec3(5.0,5.0,5.0));
+    float  r2 = dot(pos,pos);
+    //float  n1 = (sinp.y + sinp.x - 0.37);
+    float  n1 = (sinp.x*sinp.z - sinp.y*1.0);
+    //float  n2 = (sinp.y - sinp.z + 0.36);
+    float n2 = 0.0;
+    return (n1*n1+n2*n2) - iso*(1.0-r2*0.3);
+    //return r2-iso;
+}
+*/
+
+vec2 sphIntersect( in vec3 ro, in vec3 rd, in vec4 sph ){
+	vec3 oc = ro - sph.xyz;
+	float b = dot( oc, rd );
+	float c = dot( oc, oc ) - sph.w*sph.w;
+	float h = b*b - c;
+	if( h<0.0 ) return vec2(-1.0,-1.0);
+    float  sh =  sqrt( h );
+	return vec2( -b-sh, -b+sh);
+}
+
+vec2 rot(in vec2 v,in vec2 u){ return vec2(v.x*u.x-v.y*u.y,v.x*u.y+v.y*u.x); }
+
+float dirDeriv( vec3 pos, vec3 hat, float d ){ return (Func(pos+hat*d)-Func(pos))/d; }
+
+vec3 grad( vec3 pos, float d ){ 
+    float f0 = Func(pos);
+    vec2 dv = vec2(0.0,d);
+    return vec3( Func(pos+dv.yxx)-f0, Func(pos+dv.xyx)-f0, Func(pos+dv.xxy)-f0 )/d;
+}
+
+vec3 phong( vec3 rd, vec3 normal, vec3 color ){
+	//normal = normalize(normal);
+    float D = dot(light_dir,normal);
+    float S = dot(normal,normalize(light_dir-rd) )-1.0;
+    return color*( vec3(0.2,0.2,0.25)     // ambient
+         + 0.8*clamp(D,0.0,1.0) )             // difuse
+         + vec3(1.0/(S*S*25600.0+1.0));   // Specular
+}
+
+float curvatureShading( vec3 pos, vec3 normal, float d ){
+    //return ( dirDeriv(pos,normal,d) - dirDeriv(pos,normal,-d) )/d;
+    vec3 binor   = normalize(vec3(0.0,1.0,0.0));
+    vec3 u       = normalize( cross(binor,normal) );
+    vec3 v       = cross(u,normal);
+    float f0     = Func(pos);
+    float d2f_uu = Func(pos+u*d) + Func(pos+u*-d) - 2.0*f0;
+    float d2f_vv = Func(pos+u*d) + Func(pos+u*-d) - 2.0*f0;
+    return (d2f_uu + d2f_vv)/(d*d);
+}
+
+float linRoot( float t1, float dt, float f1, float f2 ){ return dt*f1/(f1-f2); }
+
+float bisec( float t1, float t2, float f1, float f2, vec3 ro, vec3 rd ){ 
+    for(int i=0; i<8; i++){
+        float t   = t1 + (t2-t1)*f1/(f1-f2);
+        float f   = Func(ro + t*rd);
+        if( f<0.0 ){t2=t;f2=f;}
+        else       {t1=t;f1=f;}
+    }
+    return t1 + (t2-t1)*f1/(f1-f2);
+}
+
+float optStep(float of, float f){
+	float dt=dt_max;
+	if( (of-f)>0.0 ){ dt = clamp(0.5*dt_max*f/(of-f),dt_min,dt_max); };
+    return dt;
+}
+
+//==== Main
+
+//void mainImage( out vec4 fragColor, in vec2 fragCoord ){
 void main( void ){
-	vec2 q    = (gl_FragCoord.xy/resolution.xy) - vec2(0.5,0.5);
-	vec3 ray0 = vec3( 0.0, 0.0, -50.0 );	 
-	vec3 hRay = vec3( q.x, q.y,  10.0 );
-	hRay = normalize( hRay ); 
 
-	float t = rayMarch( ray0, hRay );
-	vec3  hitpos = ray0 + t*hRay;
-
-	float  val0  = evalFunc ( hitpos ); 
-	vec3  normal = getNormal( hitpos, val0 );
-	normal       = normalize( normal ); if( val0<0.0 ) normal*=-1.0;
-	float c_diff = dot ( light_dir,      normal );  
-	//float c_spec = dot      ( light_dir-hRay, normal );  c_spec*=c_spec; c_spec*=c_spec; c_spec*=c_spec;
-	//float c = c_diff + c_spec;
-	float c = 0.1 + max( 0.0, c_diff );
-
-	if( t < (tmax-dtmax) ){
-		if( val0 > 0.0 ){
-			gl_FragColor = vec4( 0.0, 0.5*c,   c, 1.0 );
-		}else{
-			gl_FragColor = vec4( c,   0.5*c, 0.0, 1.0 );
-		};
-		
-	}else{
-		gl_FragColor = vec4( 0.0, 0.0, 0.0, 1.0 );
-	}
-
+    vec3 ro = camPos;
+    vec3 rd = lookAt - camPos;
+    vec3 lf = cross(rd,vec3(0.,1.,0.));  lf = normalize( lf );
+    vec3 up = cross(rd,lf);              up = normalize( up );
+    vec2 q  = ((gl_FragCoord.xy/resolution.xy) - vec2(0.5,0.5))*zoom;
+    //hRay += lf*q.x + up*q.y; // perspective
+    ro   += lf*q.x + up*q.y; // orthographic
+    rd = normalize( rd );
+    
+    vec4 sph = vec4( lookAt,  4.0 );
+    vec2 tt = sphIntersect( ro, rd, sph ); // bounding body
+    
+    vec4 clr = vec4(1.0,1.0,1.0,0.5);
+    if( tt.x>0.0 ){
+        //float t = tt.x;
+        float t = 0.0;
+        //float it = 0.0;
+        float f,of;
+        float dt = 0.1;
+        //float dt = (tt.y-tt.x)/64.0;
+        for(int i=0; i<128; i++){ // overall Ray march
+            f = Func(ro + t*rd);
+            if(f<0.0)break;
+            dt = optStep(of,f);
+            of = f;
+            t += dt; 
+            //it+= (1.0/64.0)*5.0;
+            if( (t>tt.y) ) break;
+        }
+        if(f<0.0){              // precise root and normal
+            t    = bisec( t-dt, t, of, f, ro, rd );
+            vec3 pos    = ro+rd*t;
+            vec3 normal = normalize(grad( pos, 0.001));
+            vec3 color = vec3(1.0,1.0,1.0);
+            if( LCAO(pos)>0. ){ color = vec3(0.,0.5,1.); }
+            else              { color = vec3(1.,0.5,0.); }
+            clr.rgb     = phong( rd, normal, color );
+            //clr.rgb *= 1.5-(t-2.0)*0.5;
+            //clr.rgb = color;
+            float depth = 1.0 - 1.0/(100.0 + t );
+            gl_FragDepth = depth;
+            gl_FragColor = clr;
+            return;
+        }
+    }
+    discard;
+    //gl_FragDepth = 0.9999999;
+    //gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 );
+    //discard;
+    //fragColor = clr;
+    //gl_FragColor = clr;
 }
+
+
 
 
