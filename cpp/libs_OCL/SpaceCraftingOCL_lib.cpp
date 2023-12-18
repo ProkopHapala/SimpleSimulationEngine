e09d7d19d605141d2f00cea7afa96ddb327533b5
Prokop Hapala <ProkopHapala@gmail.com>
2023-12-18 15:47:01 +0100

    modified CMake files to allow more inteligent selection of modules and dependencies

---
+++ cpp/libs_OCL/SpaceCraftingOCL_lib.cpp
@@ -0,0 +1,33 @@
+
+
+// No need to explicitely include the OpenCL headers 
+//#include <clFFT.h>
+
+#define CL_TARGET_OPENCL_VERSION 200
+
+#include <CL/cl.h>
+//#include <clFFT.h>
+#include "OCLfft_errors.h"
+
+#include "testUtils.h"
+#include "OCL.h"
+#include "IO_utils.h"
+
+int verbosity = 0;
+//#include "OCL_DFT.h"
+//#include "OCL_PP.h"
+
+//OCL_PP oclfft;
+
+//#include "FireCoreAPI.h"
+//#include "approximation.h"
+
+//OCLsystem ocl;
+//Approx::AutoApprox aaprox;
+//FireCore::Lib      fireCore;
+
+extern "C" {
+
+   
+
+};
