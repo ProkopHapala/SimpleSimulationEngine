8d0f8bf14950c40fcdbe867fe5bc3eebc770c6be
Prokop Hapala <ProkopHapala@gmail.com>
2024-01-21 13:43:50 +0100

    debugging dynamics_PBD_GS() on rope in python (try_CG.py); over-relaxation coeffs and inertia helps to converge perpendicular DOFs, but it makes convergence or parallel DOFs slower; I think perpendicular dismplacement of the rope is particularly difficult example. In practice we want constrain solver to solve displacements which are paralel to constrains; Therefore it is better to test it on some rigid truss rather than rope

---
+++ python/constrain_solver.py
@@ -0,0 +1,351 @@
+#!/usr/bin/python
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.sparse.linalg as spla
+from   matplotlib import collections  as mc
+
+# =========== Functions
+iCGstep=0
+bPrint = False
+
+def stepCG( x, d, f, f2old, K=None, dotFunc=None ):
+    global iCGstep
+    if dotFunc is not None:
+        Kd  = dotFunc(d)
+    else:
+        Kd = np.dot(K,d)
+    if bPrint:
+        print("[%i]r :" %(iCGstep-1), f  );
+        print("[%i]p :" %(iCGstep-1), d  );
+        print("[%i]Ap:" %(iCGstep-1), Kd );
+    dt  = np.dot(d,f) / np.dot(d,Kd)    # step length such that f is orthogonal to d 
+    #dt  = np.dot(f,f) / np.dot(d,Kd)
+    if bPrint:
+        print( "### CG_step ", iCGstep, " dt=", dt, " rho=", f2old )
+        print( "[%i]Kd:" %iCGstep, Kd )
+        #print( "ox", x )
+        #print( "of", f )
+        print( "dt", dt )
+    x   = x + d *dt
+    f   = f - Kd*dt
+    if bPrint:
+        print( "[%i]x :"%iCGstep, x )
+        print( "[%i]f :"%iCGstep, f )
+    f2  = np.dot(f,f)
+    #print( "CG[%i]" %iCGstep, "dt ", dt, "f2", f2, "f2old", f2old )
+    d   = f + d*(f2/f2old)
+    iCGstep = iCGstep+1
+    # NOTE: we can always normalize d to 1, but it is not necessary
+    return x, f, d, f2
+
+def SolveCG( f0, K=None, dotFunc=None, x0=None, niter=10, eps=1e-6, bPrint=False ):
+    global iCGstep
+    iCGstep = 0
+    if x0 is None: x0 = np.zeros(len(f0))
+    #print( "===== SolveCG" )
+    x  = x0.copy()
+    
+    if dotFunc is not None:
+        f = f0 - dotFunc(x)
+    else:
+        f  = f0 - np.dot(K,x)
+    d  = f.copy()
+    f2 = np.dot(f,f)
+    #if bPrint:
+    #print( "[_]x :", x )
+    #print( "[_]f0:", f0)
+    #print( "[_]f :", f )
+    #print( "[_]d :", d )
+    #    print( "---- CG loop: f2= ", f2 )
+    eps2 = eps**2
+    for i in range(niter):
+        x, f, d, f2 = stepCG( x, d, f, f2, K=K, dotFunc=dotFunc )
+        #print( "CG[%i] x: " %i, x )
+        if bPrint: print( "CG[%i] x: " %i, x )
+        if( f2 < eps2 ): 
+            if bPrint: print ("### CG converged at step", i )
+            break
+    return x, f2
+
+def Jacobi( K, f0, x0=None, niter=10, eps=1e-6 ):
+    n = len(f0)
+    if x0 is None: 
+        x = np.zeros(n)
+    else:
+        x = x0.copy()
+    for i in range(niter):
+        f2 = 0.0
+        for i in range(n):
+            fi = f0[i] - np.dot( K[i,:], x )
+            x[i] =  fi / K[i,i]
+            f2 += fi*fi
+        if( f2 < eps**2 ): break
+    return x, f2
+
+def SOR( K, f0, x0=None, niter=10, eps=1e-6, w=0.9 ):
+    n = len(f0)
+    if x0 is None: 
+        x = np.zeros(n)
+    else:
+        x = x0.copy()
+    for i in range(niter):
+        f2 = 0.0
+        for i in range(n):
+            fi = f0[i] - np.dot( K[i,:], x )
+            x[i] = x[i]*(1.-w) + fi * (w/K[i,i])
+        f2 += fi*fi
+        if( f2 < eps**2 ): break
+    return x, f2
+
+def makeNeights( sticks, npoint ):
+    neighBs = [ [] for i in range(npoint) ]
+    for i,(a,b,k) in enumerate(sticks):
+        neighBs[a].append( (b, i) )
+        neighBs[b].append( (a, i) )
+    nNeighMax = max( [len(neighBs[i]) for i in range(len(neighBs))] )
+    return neighBs, nNeighMax
+
+def constr_jacobi_neighs2_absolute(ps, neighBs, ms, l0s ):
+    '''
+     This is constrain-solver
+       we know how long should be each bond
+       we need to solve for dpos (i.e. how much we should shift each point to make the bond correct length)
+       we shift more the points which are lighter (i.e. we use inverse mass as a weight)
+       it should correspond to to position based dynamics (PBD) developed by Muller et al. 2007 (https://matthias-research.github.io/pages/) https://doi.org/10.1016/j.jvcir.2007.01.005  https://matthias-research.github.io/pages/publications/posBasedDyn.pdf
+    '''
+    dlmax = 0.0
+    nPoint = len(ps)
+    dpos = np.zeros( ps.shape )
+    for iG in range(nPoint):
+        pi = ps[iG]
+        mi = ms[iG]
+        dp = np.zeros(3)
+        wsum = 0
+        for ja,ib in neighBs[iG]:
+            if ib == -1:
+                break
+            d  = ps[ja,:] - pi
+            l  = np.linalg.norm(d)
+            dl = l - l0s[ ib ]
+            mj = ms[ja]
+            w  = mj / ( mj + mi )  # (1/mi)/(1/mi+1/mj) = mi*mj/( mi*(mi + mj)) = mj/(mi+mj)
+
+            #print( "w[%i,%i]=%g (mi=%g,mj=%g)" %( iG, ja, w, mi,mj ) )  
+            dp   += d * dl * w * w
+            wsum += w
+            dl    = np.abs(dl)
+            dlmax = max(dl, dlmax)
+        dp *= 1.0/wsum
+        dpos[iG,:] = dp
+    return dpos, dlmax
+
+def apply_sticks( dx, sticks, hdirs, constrKs=None, kReg=1e-2 ):
+
+    n = len(dx)//3
+    f = np.zeros((n*3))
+    #print( "kReg", kReg )
+    #print( "constrKs", constrKs )
+    #print( "constrKs_glob ", constrKs_glob )
+    for ia in range(n):
+        i3 = ia*3
+        k  = constrKs[ia] + kReg
+        #print( "Kdp[%i] k %g dp(%g,%g,%g)" %( ia, k, dx[i3+0], dx[i3+1], dx[i3+2] ) );
+        #k *= 0.0 # DEBUG
+        f[i3+0] = dx[i3+0]*k
+        f[i3+1] = dx[i3+1]*k
+        f[i3+2] = dx[i3+2]*k
+    
+    #print( "fdpos:", f )
+    for ib,( i,j,k) in enumerate(sticks):
+        i3 = i*3
+        j3 = j*3
+        # --- strick vector
+        h    = hdirs[ib]; #print( "hdir", hdir )
+        dxij = dx[j3:j3+3] - dx[i3:i3+3]
+        #k*= 0 # DEBUG
+        dl   =  np.dot( h, dxij )
+        fb   =  h * ( dl * -k )    # scalar force
+        #print( "dot_bond[%i] k %g dl %g h(%g,%g,%g) f(%g,%g,%g)" %( ib, k, dl, h[0],h[1],h[2],   fb[0],fb[1],fb[2]    ) );
+        # apply force to the points
+        # point i
+        f[i3+0] += fb[0]
+        f[i3+1] += fb[1]
+        f[i3+2] += fb[2]
+        # point j
+        f[j3+0] -= fb[0]
+        f[j3+1] -= fb[1]
+        f[j3+2] -= fb[2]
+    return f
+
+def globalize( sticks, hdirs ):
+    global sticks_glob, hdirs_glob #, constrKs_glob, kReg_glob
+    sticks_glob    = sticks
+    hdirs_glob     = hdirs
+    #constrKs_glob  = np.zeros(len(hdirs)//2)
+    #kReg_glob      = 1e-2
+
+def dot_func( dx ):
+    return apply_sticks( dx, sticks_glob, hdirs_glob, constrKs=constrKs_glob, kReg=kReg_glob )
+
+def stickLenghs( sticks, ps ):
+    ls = np.zeros(len(sticks))
+    for ib,( i,j,k) in enumerate(sticks):
+        x = ps[j,0] - ps[i,0]
+        y = ps[j,1] - ps[i,1]
+        z = ps[j,2] - ps[i,2]
+        ls[ib]  = np.sqrt( x*x + y*y + z*z )
+    return ls
+
+def linearize( sticks, ps, l0s=None, constrKs=None, kReg=1e-2 ):
+    n   = len(ps)
+    fdl = np.zeros((n*3))
+    if constrKs is None: constrKs = np.zeros(n)
+    bIsRelaxed = False
+    if l0s  is None: bIsRelaxed = True
+    ls = np.zeros(len(sticks))
+
+    hdirs = np.zeros((len(sticks),3))
+    for ib,( i,j,k) in enumerate(sticks):
+
+        # --- strick vector
+        x = ps[j,0] - ps[i,0]
+        y = ps[j,1] - ps[i,1]
+        z = ps[j,2] - ps[i,2]
+        l  = np.sqrt( x*x + y*y + z*z )
+
+        # --- stick length and normalized stick direction
+        ls[ib] = l
+        il = 1./l
+        x*=il
+        y*=il
+        z*=il
+        hdirs[ib,:] = [x,y,z]
+
+        # --- force due to change of stick length
+        dl = 0.0
+        if not bIsRelaxed: 
+            dl  = ls[ib] - l0s[ib]
+        fdlij = k*dl
+
+        #print( "prepare_lin[%i|%i,%i] f0=%g l=%g hdir(%g,%g,%g)"  %( ib,i,j, fdlij, l, hdirs[ib,0], hdirs[ib,1], hdirs[ib,2]) );
+
+        # construct the system in the standard form Ax=b
+        i3 = i*3
+        j3 = j*3
+        
+        # force vector ( apply sticks stress to the points )
+        fdl[i3+0] += x*fdlij
+        fdl[i3+1] += y*fdlij
+        fdl[i3+2] += z*fdlij
+        fdl[j3+0] -= x*fdlij
+        fdl[j3+1] -= y*fdlij
+        fdl[j3+2] -= z*fdlij
+        
+    return fdl, ls, hdirs
+
+def makeMat( sticks, ps, l0s=None, constrKs=None, kReg=1e-2 ):
+    '''
+    sticks:   list of (i,j,k) where k is the spring constant
+    ps:       list of (x,y) coordinates of the points
+    constrKs: list of spring constants constraining the points in place (i.e. fixed points), this is important to ensure that the matrix is well conditioned
+    kReg:     regularization constant to ensure that the matrix is well conditioned (e.g. if there are no constrKs)
+    '''
+    n   = len(ps)
+    fdl = np.zeros((n*3))
+    A   = np.zeros((n*3,n*3))
+    A  += np.diag( np.ones(3*n)*kReg )   # regularization, so that the matrix is well conditioned and points does not move too much from their original position
+    if constrKs is None: constrKs = np.zeros(n)
+    bIsRelaxed = False
+    if l0s  is None: bIsRelaxed = True
+    ls = np.zeros(len(sticks))
+
+    for i in range(3*n):
+        A[i,i] += constrKs[i//3]
+
+    #A[:,:] = 0.0 # DEBUG
+
+    #print( "kReg", kReg )
+    #print( "constrKs", constrKs )
+
+    hdirs = np.zeros((len(sticks),3))
+    
+    for ib,( i,j,k) in enumerate(sticks):
+
+        # --- strick vector
+        x = ps[j,0] - ps[i,0]
+        y = ps[j,1] - ps[i,1]
+        z = ps[j,2] - ps[i,2]
+        l  = np.sqrt( x*x + y*y + z*z )
+
+        # --- stick length and normalized stick direction
+        ls[ib] = l
+        il = 1./l
+        x*=il
+        y*=il
+        z*=il
+        hdirs[ib,:] = [x,y,z]
+
+        # --- force due to change of stick length
+        dl = 0.0
+        if not bIsRelaxed: 
+            dl  = ls[ib] - l0s[ib]
+        fdlij = k*dl
+
+        # construct the system in the standard form Ax=b
+        i3 = i*3
+        j3 = j*3
+        
+        # force vector ( apply sticks stress to the points )
+        fdl[i3+0] += x*fdlij
+        fdl[i3+1] += y*fdlij
+        fdl[i3+2] += z*fdlij
+        fdl[j3+0] -= x*fdlij
+        fdl[j3+1] -= y*fdlij
+        fdl[j3+2] -= z*fdlij
+        
+
+        #k*=0.0 # DEBUG
+        # --- stiffness matrix
+        # diagonal i,i
+        A[i3+0,i3+0] += k*x*x
+        A[i3+1,i3+0] += k*y*x
+        A[i3+2,i3+0] += k*z*x
+        A[i3+0,i3+1] += k*x*y
+        A[i3+1,i3+1] += k*y*y
+        A[i3+2,i3+1] += k*z*y
+        A[i3+0,i3+2] += k*x*z
+        A[i3+1,i3+2] += k*y*z
+        A[i3+2,i3+2] += k*z*z
+        # diagonal j,j
+        A[j3+0,j3+0] += k*x*x
+        A[j3+1,j3+0] += k*y*x
+        A[j3+2,j3+0] += k*z*x
+        A[j3+0,j3+1] += k*x*y
+        A[j3+1,j3+1] += k*y*y
+        A[j3+2,j3+1] += k*z*y
+        A[j3+0,j3+2] += k*x*z
+        A[j3+1,j3+2] += k*y*z
+        A[j3+2,j3+2] += k*z*z
+        # off-diagonal i,j
+        A[i3+0,j3+0] -= k*x*x
+        A[i3+1,j3+0] -= k*y*x
+        A[i3+2,j3+0] -= k*z*x
+        A[i3+0,j3+1] -= k*x*y
+        A[i3+1,j3+1] -= k*y*y
+        A[i3+2,j3+1] -= k*z*y
+        A[i3+0,j3+2] -= k*x*z
+        A[i3+1,j3+2] -= k*y*z
+        A[i3+2,j3+2] -= k*z*z
+        # off-diagonal j,i
+        A[j3+0,i3+0] -= k*x*x
+        A[j3+1,i3+0] -= k*y*x
+        A[j3+2,i3+0] -= k*z*x
+        A[j3+0,i3+1] -= k*x*y
+        A[j3+1,i3+1] -= k*y*y
+        A[j3+2,i3+1] -= k*z*y
+        A[j3+0,i3+2] -= k*x*z
+        A[j3+1,i3+2] -= k*y*z
+        A[j3+2,i3+2] -= k*z*z
+    return A, fdl, ls, hdirs
--- python/try_CG.py
+++ python/try_CG.py
@@ -6,299 +6,92 @@ import matplotlib.pyplot as plt
 import scipy.sparse.linalg as spla
 from   matplotlib import collections  as mc
 
-# =========== Functions
-iCGstep=0
-bPrint = False
+import constrain_solver as cs
 
-def stepCG( x, d, f, f2old, K=None, dotFunc=None ):
-    global iCGstep
-    if dotFunc is not None:
-        Kd  = dotFunc(d)
-    else:
-        Kd = np.dot(K,d)
-    if bPrint:
-        print("[%i]r :" %(iCGstep-1), f  );
-        print("[%i]p :" %(iCGstep-1), d  );
-        print("[%i]Ap:" %(iCGstep-1), Kd );
-    dt  = np.dot(d,f) / np.dot(d,Kd)    # step length such that f is orthogonal to d 
-    #dt  = np.dot(f,f) / np.dot(d,Kd)
-    if bPrint:
-        print( "### CG_step ", iCGstep, " dt=", dt, " rho=", f2old )
-        print( "[%i]Kd:" %iCGstep, Kd )
-        #print( "ox", x )
-        #print( "of", f )
-        print( "dt", dt )
-    x   = x + d *dt
-    f   = f - Kd*dt
-    if bPrint:
-        print( "[%i]x :"%iCGstep, x )
-        print( "[%i]f :"%iCGstep, f )
-    f2  = np.dot(f,f)
-    #print( "CG[%i]" %iCGstep, "dt ", dt, "f2", f2, "f2old", f2old )
-    d   = f + d*(f2/f2old)
-    iCGstep = iCGstep+1
-    # NOTE: we can always normalize d to 1, but it is not necessary
-    return x, f, d, f2
+def dynamics_PBD_GS( ps, ms, f0, sticks, niter=1, dt=0.5, nsolvmax=10, dlconv=1e-3 ):
+    cmap   = plt.get_cmap('rainbow')
+    colors = [cmap(i/float(nsolvmax)) for i in range(nsolvmax)]
+    n = len(ps)
+    plt.figure(figsize=(3*niter,3))
+    plt.subplot(1,niter,1)
+    plt.plot(   ps[:,0], ps[:,1], 'o-k' )
 
-def SolveCG( f0, K=None, dotFunc=None, x0=None, niter=10, eps=1e-6, bPrint=False ):
-    global iCGstep
-    iCGstep = 0
-    if x0 is None: x0 = np.zeros(len(f0))
-    #print( "===== SolveCG" )
-    x  = x0.copy()
-    
-    if dotFunc is not None:
-        f = f0 - dotFunc(x)
-    else:
-        f  = f0 - np.dot(K,x)
-    d  = f.copy()
-    f2 = np.dot(f,f)
-    #if bPrint:
-    #print( "[_]x :", x )
-    #print( "[_]f0:", f0)
-    #print( "[_]f :", f )
-    #print( "[_]d :", d )
-    #    print( "---- CG loop: f2= ", f2 )
-    eps2 = eps**2
+    l0s                = cs.stickLenghs( sticks, ps       )
+    neighBs, nNeighMax = cs.makeNeights( sticks, npoint=n )
+    v = np.zeros((n,3))
+
+    vdp = np.zeros((n,3))
+
+    #print( "l0s", l0s )
+    #print( "ms",  ms  )
+    #for i,b in enumerate(neighBs): 
+    #    print("neighBs[%i]" %i, b)
+    #exit()
+
+    f0[2,1] = -5.0
+    f0[2,0] =  5.0
+
+    dp_sc = 1.2
+    sc_1  = 2.0
+    #bmix  = 0.7
+    bmix_anti = 0.0
+    bmix_syn  = 1.0
+
     for i in range(niter):
-        x, f, d, f2 = stepCG( x, d, f, f2, K=K, dotFunc=dotFunc )
-        #print( "CG[%i] x: " %i, x )
-        if bPrint: print( "CG[%i] x: " %i, x )
-        if( f2 < eps2 ): 
-            if bPrint: print ("### CG converged at step", i )
-            break
-    return x, f2
-
-def Jacobi( K, f0, x0=None, niter=10, eps=1e-6 ):
-    n = len(f0)
-    if x0 is None: 
-        x = np.zeros(n)
-    else:
-        x = x0.copy()
-    for i in range(niter):
-        f2 = 0.0
-        for i in range(n):
-            fi = f0[i] - np.dot( K[i,:], x )
-            x[i] =  fi / K[i,i]
-            f2 += fi*fi
-        if( f2 < eps**2 ): break
-    return x, f2
-
-def SOR( K, f0, x0=None, niter=10, eps=1e-6, w=0.9 ):
-    n = len(f0)
-    if x0 is None: 
-        x = np.zeros(n)
-    else:
-        x = x0.copy()
-    for i in range(niter):
-        f2 = 0.0
-        for i in range(n):
-            fi = f0[i] - np.dot( K[i,:], x )
-            x[i] = x[i]*(1.-w) + fi * (w/K[i,i])
-        f2 += fi*fi
-        if( f2 < eps**2 ): break
-    return x, f2
-
-def apply_sticks( dx, sticks, hdirs, constrKs=None, kReg=1e-2 ):
-
-    n = len(dx)//3
-    f = np.zeros((n*3))
-    #print( "kReg", kReg )
-    #print( "constrKs", constrKs )
-    #print( "constrKs_glob ", constrKs_glob )
-    for ia in range(n):
-        i3 = ia*3
-        k  = constrKs[ia] + kReg
-        #print( "Kdp[%i] k %g dp(%g,%g,%g)" %( ia, k, dx[i3+0], dx[i3+1], dx[i3+2] ) );
-        #k *= 0.0 # DEBUG
-        f[i3+0] = dx[i3+0]*k
-        f[i3+1] = dx[i3+1]*k
-        f[i3+2] = dx[i3+2]*k
-    
-    #print( "fdpos:", f )
-    for ib,( i,j,k) in enumerate(sticks):
-        i3 = i*3
-        j3 = j*3
-        # --- strick vector
-        h    = hdirs[ib]; #print( "hdir", hdir )
-        dxij = dx[j3:j3+3] - dx[i3:i3+3]
-        #k*= 0 # DEBUG
-        dl   =  np.dot( h, dxij )
-        fb   =  h * ( dl * -k )    # scalar force
-        #print( "dot_bond[%i] k %g dl %g h(%g,%g,%g) f(%g,%g,%g)" %( ib, k, dl, h[0],h[1],h[2],   fb[0],fb[1],fb[2]    ) );
-        # apply force to the points
-        # point i
-        f[i3+0] += fb[0]
-        f[i3+1] += fb[1]
-        f[i3+2] += fb[2]
-        # point j
-        f[j3+0] -= fb[0]
-        f[j3+1] -= fb[1]
-        f[j3+2] -= fb[2]
-    return f
-
-def globalize( sticks, hdirs ):
-    global sticks_glob, hdirs_glob #, constrKs_glob, kReg_glob
-    sticks_glob    = sticks
-    hdirs_glob     = hdirs
-    #constrKs_glob  = np.zeros(len(hdirs)//2)
-    #kReg_glob      = 1e-2
-
-def dot_func( dx ):
-    return apply_sticks( dx, sticks_glob, hdirs_glob, constrKs=constrKs_glob, kReg=kReg_glob )
-
-def linearize( sticks, ps, l0s=None, constrKs=None, kReg=1e-2 ):
-    n   = len(ps)
-    fdl = np.zeros((n*3))
-    if constrKs is None: constrKs = np.zeros(n)
-    bIsRelaxed = False
-    if l0s  is None: bIsRelaxed = True
-    ls = np.zeros(len(sticks))
-
-    hdirs = np.zeros((len(sticks),3))
-    for ib,( i,j,k) in enumerate(sticks):
-
-        # --- strick vector
-        x = ps[j,0] - ps[i,0]
-        y = ps[j,1] - ps[i,1]
-        z = ps[j,2] - ps[i,2]
-        l  = np.sqrt( x*x + y*y + z*z )
-
-        # --- stick length and normalized stick direction
-        ls[ib] = l
-        il = 1./l
-        x*=il
-        y*=il
-        z*=il
-        hdirs[ib,:] = [x,y,z]
-
-        # --- force due to change of stick length
-        dl = 0.0
-        if not bIsRelaxed: 
-            dl  = ls[ib] - l0s[ib]
-        fdlij = k*dl
-
-        #print( "prepare_lin[%i|%i,%i] f0=%g l=%g hdir(%g,%g,%g)"  %( ib,i,j, fdlij, l, hdirs[ib,0], hdirs[ib,1], hdirs[ib,2]) );
-
-        # construct the system in the standard form Ax=b
-        i3 = i*3
-        j3 = j*3
+        print( "--------- move[%i]" %i );
+        plt.subplot(1,niter,i+1)
         
-        # force vector ( apply sticks stress to the points )
-        fdl[i3+0] += x*fdlij
-        fdl[i3+1] += y*fdlij
-        fdl[i3+2] += z*fdlij
-        fdl[j3+0] -= x*fdlij
-        fdl[j3+1] -= y*fdlij
-        fdl[j3+2] -= z*fdlij
+        v[:,:]  += f0[:,:]*dt   
+        ps_bak = ps.copy()
+        plt.plot( ps_bak[:,0], ps_bak[:,1], 'o--k', label=("start[%i]" % i) )
+        ps[:,:] += v[:,:]*dt   
+        ps_mov = ps.copy()
+        plt.plot( ps_mov[:,0], ps_mov[:,1], 'o:k',  label=("moved[%i]" % i) )
         
-    return fdl, ls, hdirs
-
-
-def makeMat( sticks, ps, l0s=None, constrKs=None, kReg=1e-2 ):
-    '''
-    sticks:   list of (i,j,k) where k is the spring constant
-    ps:       list of (x,y) coordinates of the points
-    constrKs: list of spring constants constraining the points in place (i.e. fixed points), this is important to ensure that the matrix is well conditioned
-    kReg:     regularization constant to ensure that the matrix is well conditioned (e.g. if there are no constrKs)
-    '''
-    n   = len(ps)
-    fdl = np.zeros((n*3))
-    A   = np.zeros((n*3,n*3))
-    A  += np.diag( np.ones(3*n)*kReg )   # regularization, so that the matrix is well conditioned and points does not move too much from their original position
-    if constrKs is None: constrKs = np.zeros(n)
-    bIsRelaxed = False
-    if l0s  is None: bIsRelaxed = True
-    ls = np.zeros(len(sticks))
-
-    for i in range(3*n):
-        A[i,i] += constrKs[i//3]
-
-    #A[:,:] = 0.0 # DEBUG
-
-    #print( "kReg", kReg )
-    #print( "constrKs", constrKs )
-
-    hdirs = np.zeros((len(sticks),3))
-    
-    for ib,( i,j,k) in enumerate(sticks):
-
-        # --- strick vector
-        x = ps[j,0] - ps[i,0]
-        y = ps[j,1] - ps[i,1]
-        z = ps[j,2] - ps[i,2]
-        l  = np.sqrt( x*x + y*y + z*z )
-
-        # --- stick length and normalized stick direction
-        ls[ib] = l
-        il = 1./l
-        x*=il
-        y*=il
-        z*=il
-        hdirs[ib,:] = [x,y,z]
-
-        # --- force due to change of stick length
-        dl = 0.0
-        if not bIsRelaxed: 
-            dl  = ls[ib] - l0s[ib]
-        fdlij = k*dl
-
-        # construct the system in the standard form Ax=b
-        i3 = i*3
-        j3 = j*3
+        vdp[:,:] = 0.0
         
-        # force vector ( apply sticks stress to the points )
-        fdl[i3+0] += x*fdlij
-        fdl[i3+1] += y*fdlij
-        fdl[i3+2] += z*fdlij
-        fdl[j3+0] -= x*fdlij
-        fdl[j3+1] -= y*fdlij
-        fdl[j3+2] -= z*fdlij
-        
+        for isolv in range(nsolvmax):
+            dps, dlmax = cs.constr_jacobi_neighs2_absolute( ps, neighBs, ms, l0s )
+            
+            #vdp[:,:] = dps[:,:]*1.5
 
-        #k*=0.0 # DEBUG
-        # --- stiffness matrix
-        # diagonal i,i
-        A[i3+0,i3+0] += k*x*x
-        A[i3+1,i3+0] += k*y*x
-        A[i3+2,i3+0] += k*z*x
-        A[i3+0,i3+1] += k*x*y
-        A[i3+1,i3+1] += k*y*y
-        A[i3+2,i3+1] += k*z*y
-        A[i3+0,i3+2] += k*x*z
-        A[i3+1,i3+2] += k*y*z
-        A[i3+2,i3+2] += k*z*z
-        # diagonal j,j
-        A[j3+0,j3+0] += k*x*x
-        A[j3+1,j3+0] += k*y*x
-        A[j3+2,j3+0] += k*z*x
-        A[j3+0,j3+1] += k*x*y
-        A[j3+1,j3+1] += k*y*y
-        A[j3+2,j3+1] += k*z*y
-        A[j3+0,j3+2] += k*x*z
-        A[j3+1,j3+2] += k*y*z
-        A[j3+2,j3+2] += k*z*z
-        # off-diagonal i,j
-        A[i3+0,j3+0] -= k*x*x
-        A[i3+1,j3+0] -= k*y*x
-        A[i3+2,j3+0] -= k*z*x
-        A[i3+0,j3+1] -= k*x*y
-        A[i3+1,j3+1] -= k*y*y
-        A[i3+2,j3+1] -= k*z*y
-        A[i3+0,j3+2] -= k*x*z
-        A[i3+1,j3+2] -= k*y*z
-        A[i3+2,j3+2] -= k*z*z
-        # off-diagonal j,i
-        A[j3+0,i3+0] -= k*x*x
-        A[j3+1,i3+0] -= k*y*x
-        A[j3+2,i3+0] -= k*z*x
-        A[j3+0,i3+1] -= k*x*y
-        A[j3+1,i3+1] -= k*y*y
-        A[j3+2,i3+1] -= k*z*y
-        A[j3+0,i3+2] -= k*x*z
-        A[j3+1,i3+2] -= k*y*z
-        A[j3+2,i3+2] -= k*z*z
-    return A, fdl, ls, hdirs
+            cvf = 0        
+            # if isolv==0:
+            #     vdp[:,:]= dps*sc_1
+            # else:
+            #     dps *= dp_sc
+            #     #vdp *= bmix
+            #     vdp *= 0.5
+            #     #vdp *= 0
+            #     vdp += dps
+            # cvf = 0
+            if isolv==0:
+                vdp[:,:]= dps*sc_1
+            else:
+                # vv  = np.dot( dps.flat, dps.flat )
+                # dd  = np.dot( vdp.flat, vdp.flat )               
+                cvf = np.dot( dps.flat, vdp.flat ) #/np.sqrt(vv*dd)
+                # #vdp += dps*(-cvf/dd)
+                # vdp *=0.5
+                # vdp += dps*dp_sc
+                if cvf<0:
+                    vdp *= bmix_anti
+                    vdp += dps*dp_sc
+                else:
+                    vdp *= bmix_syn
+                    vdp += dps*sc_1
+
+            print( "solve[%i] slmax=%g cvf=%g" %(isolv, dlmax, cvf) )
+
+            plt.quiver( ps[:,0], ps[:,1], vdp[:,0], vdp[:,1], scale=1., scale_units='xy', width=0.002 )
+            #plt.quiver( ps[:,0], ps[:,1], dps[:,0], dps[:,1] )
+            ps[:,:] += vdp
+            plt.plot( ps    [:,0], ps[:,1], 'o-', c=colors[isolv], label=("final[%i|%i]" %(i,isolv)) )
+            if dlmax<dlconv: break
+        v = (ps-ps_bak)/dt
+
+        plt.legend( loc='upper left' )
+        plt.axis('equal')
 
 def dynamics( ps, f0, niter = 3, dt=0.05, constrKs=None, kReg=5.0, bUseCG=True ):
     #cmap   = plt.get_cmap('rainbow')
@@ -312,7 +105,7 @@ def dynamics( ps, f0, niter = 3, dt=0.05, constrKs=None, kReg=5.0, bUseCG=True )
     if constrKs is None: constrKs = np.zeros(n)
     #constrKs=np.array([50.0, 0.0, 0.0, 0.0,50.0])
     ps0 = ps.copy()
-    _, _, l0s, hdirs = makeMat( sticks, ps, constrKs=constrKs, kReg=kReg )  
+    _, _, l0s, hdirs = cs.makeMat( sticks, ps, constrKs=constrKs, kReg=kReg )  
     
     global constrKs_glob, kReg_glob, sticks_glob, hdirs_glob
     constrKs_glob  = constrKs
@@ -331,7 +124,6 @@ def dynamics( ps, f0, niter = 3, dt=0.05, constrKs=None, kReg=5.0, bUseCG=True )
         print( "--------- move[%i]" %i );
         plt.subplot(1,niter,i+1)
         clr = colors[i%len(colors)]
-
 
         # ---- Predictor step ( move mass points by external forces )
         # Here we do normal dynamical move v+=(f/m)*dt, p+=v*dt
@@ -352,17 +144,17 @@ def dynamics( ps, f0, niter = 3, dt=0.05, constrKs=None, kReg=5.0, bUseCG=True )
             # dp, err2 = SolveCG( f, niter=10, eps=1e-6, bPrint=True, K=K )
 
             #print( "linearize ps", ps.flat.copy() )
-            fdl, ls, hdirs = linearize( sticks, ps, l0s=l0s, constrKs=constrKs, kReg=kReg )
+            fdl, ls, hdirs = cs.linearize( sticks, ps, l0s=l0s, constrKs=constrKs, kReg=kReg )
             #print( "fdl", fdl )
             f = f0.flatten() + fdl
             print( "f(Ax=f)", f )
             hdirs_glob = hdirs
-            dp, err2 = SolveCG( f, niter=10, eps=1e-6, bPrint=False, dotFunc=dot_func )
+            dp, err2 = cs.SolveCG( f, niter=10, eps=1e-6, bPrint=False, dotFunc=dot_func )
             print( "dp ", dp )
 
             #exit()
         else:
-            K, fdl, ls, hdirs = makeMat( sticks, ps, l0s=l0s, constrKs=constrKs, kReg=kReg )      # fixed end points
+            K, fdl, ls, hdirs = cs.makeMat( sticks, ps, l0s=l0s, constrKs=constrKs, kReg=kReg )      # fixed end points
             f  = f0.flatten() + fdl
             dp = np.linalg.solve( K, f )    # solve (f0x+fdlx) = Kx*dx    aka b=A*x ( A=Kx, x=dx, b=f0x+fdlx )
             #dp, err2 = SolveCG( f, niter=10, eps=1e-6, bPrint=False, K=K )
@@ -391,11 +183,13 @@ np.set_printoptions(linewidth=np.inf)
 
 ps = np.array([     
     [-2.0, -0.0, 0.0],
-    [-1.0, -0.1, 0.0],
-    [ 0.0, -0.2, 0.0],
-    [+1.0, -0.1, 0.0],
+    [-1.0, -0.0, 0.0],
+    [ 0.0, -0.0, 0.0],
+    [+1.0, -0.0, 0.0],
     [+2.0, -0.0, 0.0],
 ])
+
+ms = np.array([ 1.0e+300, 1.0, 1.0, 1.0, 1.0e+300 ])
 
 k0 = 50.0
 sticks =[
@@ -416,8 +210,11 @@ f0 = np.array([
 constrKs = np.array([50.0, 0.0, 0.0, 0.0,50.0])
 kReg     = 5.0
 
+# dynamics( ps, f0, constrKs=constrKs, kReg=kReg )
 
-dynamics( ps, f0, constrKs=constrKs, kReg=kReg )
+dynamics_PBD_GS( ps, ms, f0, sticks )
+
+
 
 '''
 #constrKs = [0.0, 0.0, 0.0, 0.0,0.0]
@@ -449,6 +246,6 @@ print( "x_CG ", x )
 print( "x_ref", np.linalg.solve( K, f0.flat+fdl ) )
 '''
 
-
+#plt.legend()
 plt.savefig( "try_Linearized_Move.png", bbox_inches='tight' )
 plt.show()
